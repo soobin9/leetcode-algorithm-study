7803bfa720fd1cf2c5b48c714441702bfffc4c31
soobin9 <120175855+soobin9@users.noreply.github.com>
2023-09-30 00:23:52 +0900

    Time: 179 ms (21.93%), Space: 21.6 MB (39.07%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -13,20 +13,38 @@ class Solution(object):
         #             results[i] = results[i] * n
                     
         # 2nd try
-        v = 1 
-        for n in nums:
-            v = v * n 
+#         v = 1 
+#         for n in nums:
+#             v = v * n 
         
-        results = [1 for _ in range(len(nums))]
+#         results = [1 for _ in range(len(nums))]
+#         for i, n in enumerate(nums):
+#             # 0인 경우에는 예외 처리 
+#             if n != 0 :
+#                 results[i] = v / n 
+#             else:
+#                 for j, n in enumerate(nums):
+#                     if j != i :
+#                         results[i] = results[i] * n
+        
+        # 3rd try - youtube - https://www.youtube.com/watch?v=bNvIQI2wAjk
+        # 앞에서부터 곱한 값 
+        prev = [1 for _ in range(len(nums) + 1)]
+        p = 1 
         for i, n in enumerate(nums):
-            # 0인 경우에는 예외 처리 
-            if n != 0 :
-                results[i] = v / n 
-            else:
-                for j, n in enumerate(nums):
-                    if j != i :
-                        results[i] = results[i] * n
+            prev[i + 1] = p * n 
+            p = prev[i + 1]
         
-                    
+        # 뒤에서부터 곱한 값 
+        suff = [1 for _ in range(len(nums) + 1)]
+        s = 1 
+        for i, n in enumerate(nums[::-1]):
+            suff[i + 1] = s * n 
+            s = suff[i + 1]
+            
+        results = []
+        for i, n in enumerate(nums):
+            results.append(prev[i] * suff[len(nums) - i - 1])
+        
             
         return results
