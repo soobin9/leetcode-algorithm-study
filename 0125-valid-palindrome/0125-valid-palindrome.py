b9e3a0121e780f94c902a6436595b4f2d086fab3
soobin9 <120175855+soobin9@users.noreply.github.com>
2023-10-23 23:19:27 +0900

    Time: 295 ms (19.82%), Space: 14.7 MB (52.95%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,14 +1,23 @@
 class Solution(object):
     def isPalindrome(self, s):
         
-        testString = [] 
+        # 1nd 
+#         testString = [] 
+#         for c in s:
+#             if c.isalnum():
+#                 testString.append(c.lower())
+        
+#         for i,c in enumerate(testString):
+#             if c != testString[-i-1]:
+#                 return False
+            
+        # 2st 
+        newStr = "" 
         for c in s:
             if c.isalnum():
-                testString.append(c.lower())
+                newStr += c.lower()
+        return newStr == newStr[::-1]
         
-        for i,c in enumerate(testString):
-            if c != testString[-i-1]:
-                return False
         
         return True
         
