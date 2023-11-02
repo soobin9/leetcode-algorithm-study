ddef25f7d108baeb9e9c25786546525005435ec2
soobin9 <120175855+soobin9@users.noreply.github.com>
2023-11-03 00:48:10 +0900

    Time: 10 ms (90.40%), Space: 13.3 MB (83.59%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -5,26 +5,41 @@ class Solution(object):
         :rtype: bool
         """
         
-        stack = []
-        for c in s:
-            if c in ['(', '{', '[']:
-                stack.append(c)
-            elif c in [')', '}', ']']:
-                # pop할 값이 없다면 
-                if len(stack) == 0:
-                    return False 
+#         stack = []
+#         for c in s:
+#             if c in ['(', '{', '[']:
+#                 stack.append(c)
+#             elif c in [')', '}', ']']:
+#                 # pop할 값이 없다면 
+#                 if len(stack) == 0:
+#                     return False 
                 
-                t = stack.pop()
-                if c == ')' and t != '(':
-                    return False 
-                elif c == '}' and t != '{':
-                    return False 
-                elif c == ']' and t != '[':
-                    return False 
+#                 t = stack.pop()
+#                 if c == ')' and t != '(':
+#                     return False 
+#                 elif c == '}' and t != '{':
+#                     return False 
+#                 elif c == ']' and t != '[':
+#                     return False 
         
-        # 모두다 pop 되어있다면 
-        if len(stack) == 0:
-            return True 
+#         # 모두다 pop 되어있다면 
+#         if len(stack) == 0:
+#             return True 
+
+        # 2nd 
+        stack = []
+        closeToOpen = {")": "(", "]": "[", "}": "{"}
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        
+        # 비어있으면 true 로 리턴 
+        return True if not stack else False
         
         return False
             
