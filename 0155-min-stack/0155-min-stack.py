611a6499dcc65f6e0980f00761b8ec2f3c34a160
soobin9 <120175855+soobin9@users.noreply.github.com>
2023-11-06 23:19:24 +0900

    Time: 44 ms (35.91%), Space: 16.7 MB (94.54%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,19 +1,52 @@
 class MinStack(object):
 
+#     def __init__(self):
+#         self.minStack = [sys.maxsize] 
+#         self.stack = [] 
+
+#     def push(self, val):
+#         """
+#         :type val: int
+#         :rtype: None
+#         """
+#         if (self.minStack[-1] > val):
+#             self.minStack.append(val)
+#         else:
+#             self.minStack.append(self.minStack[-1])
+#         self.stack.append(val)
+        
+
+#     def pop(self):
+#         """
+#         :rtype: None
+#         """
+#         self.stack.pop()
+#         self.minStack.pop()
+
+#     def top(self):
+#         """
+#         :rtype: int
+#         """
+#         return self.stack[-1]
+
+#     def getMin(self):
+#         """
+#         :rtype: int
+#         """
+#         return self.minStack[-1]
+        
     def __init__(self):
-        self.minStack = [sys.maxsize] 
-        self.stack = [] 
+        self.stack = []
+        self.minStack = [] 
 
     def push(self, val):
         """
         :type val: int
         :rtype: None
         """
-        if (self.minStack[-1] > val):
-            self.minStack.append(val)
-        else:
-            self.minStack.append(self.minStack[-1])
         self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val) # minStack 값이 있을때만 
+        self.minStack.append(val)
         
 
     def pop(self):
