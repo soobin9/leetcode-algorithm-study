86a837bed6c1439b45476ac880ebf056d5b9d4c0
soobin9 <120175855+soobin9@users.noreply.github.com>
2023-11-07 23:15:18 +0900

    Time: 38 ms (82.03%), Space: 15.3 MB (48.79%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -5,24 +5,41 @@ class Solution(object):
         :rtype: int
         """
         
-        stack = [] 
-        for t in tokens:
-            if t not in ['/', '+', '-', '*']:
-                stack.append(t)
-            else : 
-                n1 = int(stack.pop())
-                n2 = int(stack.pop())
+#         stack = [] 
+#         for t in tokens:
+#             if t not in ['/', '+', '-', '*']:
+#                 stack.append(t)
+#             else : 
+#                 n1 = int(stack.pop())
+#                 n2 = int(stack.pop())
                 
-                if t == '/':
-                    check = math.trunc(float(n2) / float(n1))
-                    stack.append(check)
-                elif t == '+':
-                    stack.append(n2+n1)
-                elif t == '-':
-                    stack.append(n2-n1)
-                elif t == '*':
-                    stack.append(n2*n1)
-            # print(stack)
+#                 if t == '/':
+#                     check = math.trunc(float(n2) / float(n1))
+#                     stack.append(check)
+#                 elif t == '+':
+#                     stack.append(n2+n1)
+#                 elif t == '-':
+#                     stack.append(n2-n1)
+#                 elif t == '*':
+#                     stack.append(n2*n1)
+#             # print(stack)
         
-        return int(stack.pop())
+#         return int(stack.pop())
+
+        stack = [] 
+        for c in tokens:
+            if c == '+':
+                stack.append(stack.pop() + stack.pop())
+            elif c == '-':
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif c == '*':
+                stack.append(stack.pop() * stack.pop())
+            elif c == '/':
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(float(b) / a))
+            else:
+                stack.append(int(c))
+    
+        return stack[0]
         
