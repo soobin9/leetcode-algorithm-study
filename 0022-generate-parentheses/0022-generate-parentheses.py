e0cee7556fd847b15480eb1ccb91656518714baa
soobin9 <120175855+soobin9@users.noreply.github.com>
2023-11-11 21:31:51 +0900

    Time: 16 ms (63.34%), Space: 13.6 MB (11.01%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,28 +1,62 @@
 class Solution(object):
-    res = [] 
+#     res = [] 
     
-    def dfs(self, current, n):
-        openCnt = current.count("(")
-        closeCnt = current.count(")")
+#     def dfs(self, current, n):
+#         openCnt = current.count("(")
+#         closeCnt = current.count(")")
         
-        if len(current) == 2 * n:
-            if openCnt == n and closeCnt == n:
-                self.res.append(current)
-            return;
-        else:            
-            # close 갯수 < open 갯수 
-            if closeCnt <= openCnt:
-                self.dfs(current + "(", n)
-                self.dfs(current + ")", n)
+#         if len(current) == 2 * n:
+#             if openCnt == n and closeCnt == n:
+#                 self.res.append(current)
+#             return;
+#         else:            
+#             # close 갯수 < open 갯수 
+#             if closeCnt <= openCnt:
+#                 self.dfs(current + "(", n)
+#                 self.dfs(current + ")", n)
     
+#     def generateParenthesis(self, n):
+#         """
+#         :type n: int
+#         :rtype: List[str]
+#         """
+#         self.res = [] # 테스트 케이스 전에 예전에 돌린 값 초기화 
+#         self.dfs("(", n)
+#         return self.res
+        
+        
+        
     def generateParenthesis(self, n):
-        """
-        :type n: int
-        :rtype: List[str]
-        """
-        self.res = [] # 테스트 케이스 전에 예전에 돌린 값 초기화 
-        self.dfs("(", n)
-        return self.res
+        # only add open paranthesis if open < n
+        # only add a closing paranthesis if closed < open 
+        # valid IIF open == closed == n 
+        
+        stack = [] 
+        res = [] 
+        
+        def backtrack(openN, closedN):
+            if openN == closedN == n:
+                res.append("".join(stack))
+                return 
+            
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closedN)
+                stack.pop()
+                
+            if closedN < openN:
+                stack.append(")")
+                backtrack(openN, closedN + 1)
+                stack.pop()
+                
+        backtrack(0,0)
+        return res
+                
+            
+            
+            
+        
+        
         
         
         
