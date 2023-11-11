cadc8c117462cc8f6ac873972342809d2e092d12
soobin9 <120175855+soobin9@users.noreply.github.com>
2023-11-12 00:03:36 +0900

    Time: 1030 ms (52.80%), Space: 30.1 MB (16.36%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,25 +1,35 @@
 class Solution(object):
     def dailyTemperatures(self, temperatures):
-        """
-        :type temperatures: List[int]
-        :rtype: List[int]
-        """
+#         """
+#         :type temperatures: List[int]
+#         :rtype: List[int]
+#         """
         
-        stack = [0]
-        res = [0] * len(temperatures)
-        checked = 1 
+#         stack = [0]
+#         res = [0] * len(temperatures)
+#         checked = 1 
         
-        while checked < len(temperatures):
-            while len(stack) > 0:
-                if temperatures[checked] > temperatures[stack[-1]]:
-                    res[stack[-1]] = checked - stack[-1]
-                    stack.pop()
-                else:
-                    break
+#         while checked < len(temperatures):
+#             while len(stack) > 0:
+#                 if temperatures[checked] > temperatures[stack[-1]]:
+#                     res[stack[-1]] = checked - stack[-1]
+#                     stack.pop()
+#                 else:
+#                     break
             
-            stack.append(checked)
-            checked += 1
+#             stack.append(checked)
+#             checked += 1
         
-        return res
-            
+#         return res
+
+          res = [0] * len(temperatures)
+          stack = [] # pair [temp, index]
+
+          for i,t in enumerate(temperatures):
+                while stack and t > stack[-1][0]:
+                    stackT, stackInd = stack.pop()
+                    res[stackInd] = (i - stackInd)
+                stack.append([t, i])     
+          return res
+
             
